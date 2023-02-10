500f859cbea69d34d6008bf84ee2ff706ad4c5a2
Hyeyeon Lee <tjsalsu@naver.com>
2023-02-11 04:51:12 +0900

    완강 jpa,aop

--- src/main/java/hello/hellospring/service/MemberService.java
+++ src/main/java/hello/hellospring/service/MemberService.java
@@ -6,9 +6,12 @@ import hello.hellospring.repository.MemoryMemberRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import javax.transaction.Transactional;
 import java.util.List;
 import java.util.Optional;
 
+
+@Transactional
 public class MemberService {
 
     private final MemberRepository memberRepository;
@@ -18,10 +21,18 @@ public class MemberService {
     }
     /**회원가입**/
     public Long join(Member member){
-        validateDuplicateMember(member); //중복회원 검증
+        long start = System.currentTimeMillis();
 
-        memberRepository.save(member);
-        return member.getId();
+        try {
+            validateDuplicateMember(member); //중복회원 검증
+
+            memberRepository.save(member);
+            return member.getId();
+        }finally {
+            long finish = System.currentTimeMillis();
+            long timeMs = finish - start;
+            System.out.println("join = "+timeMs+"ms");
+        }
     }
 
     private void validateDuplicateMember(Member member) {
